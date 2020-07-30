ba1510df8e7e89847b8919c197f4de6d4517ceaf
NiklausDev <50832817+nodbloxdev@users.noreply.github.com>
2020-07-29 20:47:58 -0400

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -12,19 +12,19 @@ module.exports = {
     
     
         let mage = new Canvas(500, 250)
-        .setColor("#ffffff")
+        .setColor("#cfcfcf")
         .addRect(0, 0, 500, 250)
-        .setColor(client.guild.me.roles.highest.color)
+        .setColor("#4fa1e0")
         .addRect(0, 0, 500, 80)
         .setColor("#ffffff")
         .setTextFont('bold 40px Impact')
         .addText("PROFILE CARD", 110, 55)
-        .setColor(client.guild.me.roles.highest.color)
+        .setColor("#4fa1e0")
         .setTextFont('bold 20px Impact') 
         .addText(`ID - ${message.author.id}`, 30, 140)
         .addText(`TAG - ${message.author.tag}`, 30, 170)
         .addText(`GUILD NAME - ${message.guild.name}`, 30, 200)
-        .setColor("#ffffff")
+        .setColor("#cfcfcf")
         .addCircle(60, 40, 33)
         .addCircularImage(await avatar.buffer(), 60, 40, 30)
         .toBuffer();
